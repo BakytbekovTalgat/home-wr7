38a916d7949fb7464775ce011a7ef90096080969
BakytbekovTalgat <127601643+BakytbekovTalgat@users.noreply.github.com>
2023-07-05 03:20:29 +0600

    Add files via upload

---
+++ Number.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default class NumberFrom extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {value: ""};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+      this.setState({value: e.target.value.toUpperCase() });
+  }
+  
+  handleSubmit(e) {
+    alert("Send message" + this.state.value)
+    e.preventDefault();
+  }
+  
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Номер телефона:
+          <input className='input-text' 
+            type="text" 
+            value={this.state.value} 
+            onChange={this.handleChange} 
+          />
+        </label>
+        <input className='input-send1' type="submit" value="Send" /> <br />       
+      </form>
+    )
+  }
+}
